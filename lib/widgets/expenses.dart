5a8af72b5d47c8f22939d9e6f4223e1ad2ff61cd
Hazem KATAIE <hazem.kataie@outlook.com>
2025-04-29 01:18:54 +0300

    Refactor expenses management by moving Expenses widget to a new file and creating ExpensesList and ExpenseItem components

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      name: 'flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      name: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('The chart'),
+            const SizedBox(height: 20),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: ListTile(
+          title: Text(expense.name),
+          subtitle: Text(expense.category.name),
+          trailing: Text('\$${expense.amount.toStringAsFixed(2)}'),
+        ),
+      ),
+    );
+  }
+}
